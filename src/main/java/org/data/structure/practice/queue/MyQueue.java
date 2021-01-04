ef54fbfc7b8324afc2bf2c352ff4c821d2277e52
Vikas Yadav <vyadav@mycom-internal.com>
2021-01-04 12:52:58 +0530

    queue implementation using linked list

---
+++ src/main/java/org/data/structure/practice/queue/MyQueue.java
@@ -0,0 +1,55 @@
+package org.data.structure.practice.queue;
+
+import java.util.NoSuchElementException;
+
+public class MyQueue<T>
+{
+   private static class QueueNode<T>
+   {
+      private T data;
+      private QueueNode<T> next;
+
+      public QueueNode(T data)
+      {
+         this.data = data;
+      }
+   }
+
+   private QueueNode<T> first;
+   private QueueNode<T> last;
+
+   public void add(T item)
+   {
+      QueueNode<T> node = new QueueNode<>(item);
+      if (last != null)
+         last.next = node;
+      last = node;
+      if (first == null)
+         first = last;
+   }
+
+   public T remove()
+   {
+      if (first == null)
+         throw new NoSuchElementException();
+      T data = first.data;
+      first = first.next;
+      if (first == null)
+         last = null;
+      return data;
+
+   }
+
+   public T peek()
+   {
+      if (first == null)
+         throw new NoSuchElementException();
+      return first.data;
+   }
+
+   public boolean isEmpty()
+   {
+      return first == null;
+
+   }
+}
